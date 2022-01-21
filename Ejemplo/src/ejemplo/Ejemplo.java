ee97106eefcceaef57d1b68ddced9ceb32c0d695
John Doe <johndoe@example.com>
2022-01-21 12:26:22 -0500

    primer commit

--- Ejemplo/src/ejemplo/Ejemplo.java
+++ Ejemplo/src/ejemplo/Ejemplo.java
@@ -5,9 +5,11 @@
  */
 package ejemplo;
 
+import org.apache.commons.lang3.RandomUtils;
+
 /**
  *
- * @author reroes
+ * @author TaisBalcazar
  */
 public class Ejemplo {
 
@@ -16,6 +18,27 @@ public class Ejemplo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int valor = 100;
+        int valorDos = 110;
+        String cadena;
+        
+        cadena = obtenerEntero(valor,valorDos);
+        System.out.printf("%s\n", cadena);
+
+
     }
-    
+
+    public static String obtenerEntero(int a, int b) {
+        int[] t = new int[10];
+        String cadena= ""; 
+        for (int i = 0; i < t.length; i++) {
+            t[i] = RandomUtils.nextInt(a, b);
+        }
+        for (int i = 0; i < t.length; i++) {
+            cadena = String.format("%s %s", cadena, t[i]);
+        }
+        return cadena;
+
+    }
+
 }
